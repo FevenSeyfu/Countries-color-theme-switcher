3f35b6adf386d41e4c6d951c6aed6f0b5b02168c
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-04 22:55:25 +0300

    setup fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        'white': 'hsl(0, 0%, 100%)', //darkmode-text,lightmode-elements
+        // light mode
+        'dark-gray': 'hsl(0, 0%, 52%)', //input
+        'very-dark-blue': 'hsl(200, 15%, 8%)', //text
+        'very-light-gray': 'hsl(200, 15%, 8%)', //backgound
+
+        // dark mode
+        'dark-blue': 'hsl(209, 23%, 22%)', //elements
+        'very-dark-blue': 'hsl(207, 26%, 17%)', //Backgound
+      },
+    },
   },
   plugins: [],
 }
