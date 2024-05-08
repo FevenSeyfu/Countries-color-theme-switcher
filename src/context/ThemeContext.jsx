2554273f43b0d3bbb0a5e58a31b1288b3522f46d
FevenSeyfu <fevenseyfu00@gmail.com>
2024-05-08 20:14:28 +0300

    Update the theme change to match the devices theme peference

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,12 +5,25 @@ const ThemeContext =createContext()
 const ThemeProvider = ({children}) => {
     const [isDarkTheme, setIsDarkTheme] = useState(() => {
         const storedTheme = window.localStorage.getItem('isDarkTheme');
-        return storedTheme ? JSON.parse(storedTheme) : false;
+        if (storedTheme !== null) {
+            return JSON.parse(storedTheme);
+        } else {
+            const systemTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+            return systemTheme || false;
+        }
     });
 
     useEffect(() => {
         window.localStorage.setItem('isDarkTheme', JSON.stringify(isDarkTheme));
     }, [isDarkTheme]);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const handleThemeChange = () => setIsDarkTheme(mediaQuery.matches);
+
+        mediaQuery.addEventListener('change', handleThemeChange);
+        return () => mediaQuery.removeEventListener('change', handleThemeChange);
+    }, []);
 
     const toggleTheme = () => {
         setIsDarkTheme((prevTheme) => !prevTheme)
