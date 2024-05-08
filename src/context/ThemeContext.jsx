0df924d3ac902baafbd3a0df158325f7bc5c0d70
FevenSeyfu <fevenseyfu00@gmail.com>
2024-05-08 19:58:46 +0300

    Store theme in local storage to prevent theme change when page refreshes

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,9 +1,16 @@
-import React,{ createContext,useContext,useState} from 'react'
+import React,{ createContext,useContext,useState,useEffect} from 'react'
 
 const ThemeContext =createContext()
 
 const ThemeProvider = ({children}) => {
-    const[isDarkTheme, setIsDarkTheme] = useState (false)
+    const [isDarkTheme, setIsDarkTheme] = useState(() => {
+        const storedTheme = window.localStorage.getItem('isDarkTheme');
+        return storedTheme ? JSON.parse(storedTheme) : false;
+    });
+
+    useEffect(() => {
+        window.localStorage.setItem('isDarkTheme', JSON.stringify(isDarkTheme));
+    }, [isDarkTheme]);
 
     const toggleTheme = () => {
         setIsDarkTheme((prevTheme) => !prevTheme)
